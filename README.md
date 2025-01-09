260fcb72f675380332c6011d633c15c6e62861bd
msabhimanue02 <msabhimanue2002@gmail.com>
2025-01-09 12:19:34 +0530

    modified README.md and added new file pgm3.js

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A repository for practicing and improving JavaScript skills with various example
 A simple program that outputs 'Hello, World!' to demonstrate the basics of JavaScript syntax,as shown in pgm1.js.
 # 2. Arithmetic Operations
 A simple program that outputs the basic arithmetic operations,as shown in pgm2.js.
+# 3.Variables
+A simple program that outputs the variables,as shown in pgm2.js.
---
+++ pgm3.js
@@ -0,0 +1,12 @@
+//Variables in JS
+//Always use const if the value should not be changed.
+//Always use const if the type should not be changed (Arrays and Objects).
+const name="Abhimanue"; //String in double or single quotes,const used  
+//Only use let if you can't use const
+let age=25; //Number
+//Only use var if you MUST support old browsers
+var isStudent=true; //Boolean
+
+console.log("Name :",name)
+console.log("Age :",age)
+console.log("Is a student :",isStudent)
